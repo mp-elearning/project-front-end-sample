63034060f1cf27783d8ca0b0c40a357d355edb1e
sagarlandge <mssagarlandge@gmail.com>
2019-04-10 19:59:56 +0800

    changes before merge sagar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, provideRoutes } from '@angular/router';
 import { BlogsComponent } from 'src/app/blogs/blogs.component';
 import { BitcoinBlogsComponent } from 'src/app/bitcoin-blogs/bitcoin-blogs.component';
 import { GreBlogsComponent } from 'src/app/gre-blogs/gre-blogs.component';
 import { BlockchainBlogsComponent } from 'src/app/blockchain-blogs/blockchain-blogs.component';
+import { VideosComponent } from './videos/videos.component';
 
 const routes: Routes = [
-
-    {path : '', component : BlogsComponent},
+ 
+    //{path : '', component : },
+    {path : 'blogs', component : BlogsComponent},
     {path : 'bitcoin', component : BitcoinBlogsComponent},
     {path : 'gre', component : GreBlogsComponent},
     {path : 'blockchain', component : BlockchainBlogsComponent},
+    {path: 'videos', component : VideosComponent}
     
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+
+//bootstrap: [ AppComponent ]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [VideosComponent,BlogsComponent,BlockchainBlogsComponent,GreBlogsComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogsComponent } from './blogs/blogs.component';
 import { BlockchainBlogsComponent } from './blockchain-blogs/blockchain-blogs.component';
 import { GreBlogsComponent } from './gre-blogs/gre-blogs.component';
 import { BitcoinBlogsComponent } from './bitcoin-blogs/bitcoin-blogs.component';
 import { EachBlogComponent } from './blogs/each-blog/each-blog.component';
-import { VideosComponent } from './videos/videos.component';
+//import { VideosComponent } from './videos/videos.component'; working
 
 
 import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
@@ -17,12 +17,12 @@ import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
 @NgModule({
   declarations: [
     AppComponent,
-    BlogsComponent,
+    //BlogsComponent,
     BlockchainBlogsComponent,
     GreBlogsComponent,
     BitcoinBlogsComponent,
     EachBlogComponent,
-    VideosComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
