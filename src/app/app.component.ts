49cb99f14264c650786e3b1bb5a8191cb7092270
palakkeni5 <palakkeni@gmail.com>
2019-04-12 16:13:57 +0530

    final changes before merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,8 +15,10 @@ import { Router } from '@angular/router';
 export class AppComponent {
 
   constructor(
-    public router: Router
+    public router: Router,
+    private angularFireAuth: AngularFireAuth
   ){
+    console.log(this.angularFireAuth.auth.currentUser)
 
   }
   title = 'angular-front-end';
